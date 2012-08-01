be794428597131263f71eb9a41126ea1fd203118
dilipd <dilipd.US@gmail.com>
2012-07-31 22:26:02 -0700

    before 5.3.4 'Pretty RSpec'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SampleApp::Application.routes.draw do
-  root to: 'static_pages#home'
-
+  root              to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
